fa7e616a5e817bcd22bab810a7a14e610fa8298d
Vasantharajan Pandian <memoriesvasanth@live.com>
2025-01-18 22:46:04 +0530

    5th Changes to GitHih

--- main.tf
+++ main.tf
@@ -114,6 +114,15 @@ resource "aws_lb_target_group" "sample_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.sample_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_listener" "sample_listener" {
