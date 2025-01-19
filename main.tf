f8e52f0130afe50e812727c6cbcf53d33c4e1635
Vasantharajan Pandian <memoriesvasanth@live.com>
2025-01-19 17:55:18 +0530

    8th Commit

--- main.tf
+++ main.tf
@@ -109,13 +109,6 @@ resource "aws_ecs_task_definition" "sample_task" {
           hostPort      = 80
         }
       ]
-      healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:80/ || exit 1"]
-        interval    = 30
-        timeout     = 5
-        retries     = 3
-        startPeriod = 60
-      }
     }
   ])
 }
